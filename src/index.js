aec5c35ff661f296619a757310717d4781b4ae4b
NikolasUntoten <nikolasr@ggaub.com>
2018-05-12 16:39:49 -0700

    added page routing
    not a literal react router, but a way to change page without changing url

--- src/index.js
+++ src/index.js
@@ -1,14 +1,63 @@
+/* Written by Nikolas Gaub, 5/12/2018
+ * This class renders the currently visible component.
+ * To add or remove pages, make changes to the pages constant under imports
+ *
+ */
+
+
+//Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+import Home from './pages/main/home';
+
+//All pages currently available for display on the website
+const pages = new Map([
+						['home', <Home />]
+					  ]);
+
+//Component to be rendered
 class App extends React.Component {
-  render () {
-    return <p> Hello React project</p>;
-  }
+	constructor() {
+		super();
+		
+		//Initialize state
+		this.state = {
+			page:'home', //Page determines rendered component
+		};
+		
+		//Bind functions
+		this.setPage = this.setPage.bind(this);
+	}
+	
+	//Changes page to given argument
+	//Made to be used as a callback function for children components
+	setPage(newPage) {
+		if (pages.has(newPage)) {
+			this.setState({
+				page:newPage,
+			});
+		} else {
+			console.error("Attempted to load page" + newPage + "; No such page exists.");
+		}
+	}
+	
+	//Renders currently visible page
+	render () {
+		if (pages.has(this.state.page)) {
+			return pages.get(this.state.page);
+			
+		} else {
+			return (
+				<p>Error Loading Page</p>
+			);
+		}
+	}
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+//Render component
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // Hot-loading
 registerServiceWorker();
---
+++ src/pages/main/home.js
@@ -0,0 +1,14 @@
+//Imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Component to be exported
+class Home extends React.Component {
+	render() {
+		return (
+			<p>Welcome to my personal webpage!</p>
+		);
+	}
+}
+
+export default Home;
