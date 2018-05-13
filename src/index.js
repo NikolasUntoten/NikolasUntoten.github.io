913fb9ac9bd0a095f075ab04536f61e3b2f7518f
NikolasUntoten <nikolasr@ggaub.com>
2018-05-13 14:40:29 -0700

    Added slideshow, cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 /* Written by Nikolas Gaub, 5/12/2018
  * This class renders the currently visible component.
  * To add or remove pages, make changes to the pages constant under imports
- *
  */
-
 
 //Imports
 import React from 'react';
@@ -11,48 +9,18 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import Home from './pages/main/home';
-
-//All pages currently available for display on the website
-const pages = new Map([
-						['home', <Home />]
-					  ]);
+import Slideshow from './components/slideshow';
 
 //Component to be rendered
 class App extends React.Component {
-	constructor() {
-		super();
-		
-		//Initialize state
-		this.state = {
-			page:'home', //Page determines rendered component
-		};
-		
-		//Bind functions
-		this.setPage = this.setPage.bind(this);
-	}
-	
-	//Changes page to given argument
-	//Made to be used as a callback function for children components
-	setPage(newPage) {
-		if (pages.has(newPage)) {
-			this.setState({
-				page:newPage,
-			});
-		} else {
-			console.error("Attempted to load page" + newPage + "; No such page exists.");
-		}
-	}
-	
 	//Renders currently visible page
 	render () {
-		if (pages.has(this.state.page)) {
-			return pages.get(this.state.page);
-			
-		} else {
-			return (
-				<p>Error Loading Page</p>
-			);
-		}
+		return (
+			<Slideshow style={{height:'500px', width:'500px'}}>
+				<Home />
+				<Home />
+			</Slideshow>
+		);
 	}
 }
 
